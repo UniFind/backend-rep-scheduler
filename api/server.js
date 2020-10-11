397cb5111b5cc75f9d7bd2bf2259748250131e3a
Kim Kiamco <kim.kiamco@menlosecurity.com>
2020-10-11 14:41:19 -0700

    created credentails for google api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import Express from 'express';
 import Helmet from 'helmet';
 import Cors from 'cors';
+import Mongoose from 'mongoose';    
 
 const server = Express();
 
@@ -10,6 +11,16 @@ server.use(Cors());
 server.use(Helmet());
 
 //establish connection to databse 
+const DB_CONNECTION = "mongodb://root:rootpassword@0.0.0.0:27017"
+
+Mongoose.connect(DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true },(error) => {
+    if(error){
+        console.log(error);
+        console.log("Failed to connect to MongaDb");
+    } else {
+        console.log("conected to db");
+    }
+});
 
 // send if server is up 
 server.get('/', (req,res) => {
